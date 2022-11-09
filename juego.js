7777ea12cb8bf52dacc7db310ddaa4b99df4b194
unknown <brunoborrello@gmail.com>
2022-11-09 13:00:04 -0300

    envio

--- juego.js
+++ juego.js
@@ -4,6 +4,21 @@ document.addEventListener('keyboard', function(evento){
     }
 });
 
+
+var imgRex, imgNube, imgCactus, imgSuelo;
+function cargaImagenes(){
+    imgRex = new Image();
+    imgNube = new Image();
+    imgCactus = new Image();
+    imgSuelo = new Image();
+
+    imgRex.src = img/imgRex.png;
+    imgNube.src = img/imgNube.png;
+    imgCactus.src = img/imgCactus.png;
+    imgSuelo.src = img/imgSuelo.png;
+}
+
+
 var ancho = 700;
 var alto = 300;
 var canvas,ctx;
@@ -11,11 +26,17 @@ var canvas,ctx;
 function inicializada(){
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
+    cargaImagenes();
+
 }
 
 function borraCanvas(){
     canvas.width = ancho;
     canvas.heigth = alto;
+}
+
+function dibujaRex(){
+    ctx.drawImage(imgRex,0,0,64,69,100,100,50,50);
 }
 
 
@@ -28,5 +49,6 @@ setInterval(function(){
 },1000/10);
 
 function principal(){
-
+    borraCanvas();
+    dibujaRex();
 }
